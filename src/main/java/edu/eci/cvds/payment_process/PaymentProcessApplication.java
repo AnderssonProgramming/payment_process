d73afd948f8f60840cd2e386a754b2df6fe123c5
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-04-15 23:31:38 -0500

    chore: update and to finish second CVDS exam

---
+++ src/main/java/edu/eci/cvds/payment_process/PaymentProcessApplication.java
@@ -0,0 +1,11 @@
+package edu.eci.cvds.payment_process;
+
+import org.springframework.boot.SpringApplication;
+
+public class PaymentProcessApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentProcessApplication.class, args);
+    }
+    
+}
